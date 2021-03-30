e70548fe0584a7b76849ccd1ca1b79f0ff6c7a49
Alex Danisz <alexdanisz@live.com>
2021-03-30 12:06:17 -0700

    Added Place Order Screen

---
+++ frontend/pages/placeorder/index.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PlaceOrderScreen from "../../screens/PlaceOrderScreen";
+
+const PlaceOrder = () => {
+    return (
+        <PlaceOrderScreen/>
+    )
+}
+
+export default PlaceOrder
+
+// export async function getServerSideProps(ctx) {
+//     const { id } = ctx.query;
+//     return {
+//       props: {
+//         id,
+//       },
+//     };
+//   }
+  
