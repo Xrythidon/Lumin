79104ac8ea34e297dbc7feeab9910fd4f50f3b91
Alex Danisz <alexdanisz@live.com>
2021-03-29 17:28:15 -0700

    Added profile route

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,13 +1,13 @@
 import express from "express";
 const router = express.Router();
-import {authUser, registerUser, getUserProfile} from "../controllers/userController.js";
+import { authUser, registerUser, getUserProfile, updateUserProfile } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 // @desc    Fetch All Products
 // @route   GET /api/products
 // @access  Public
-router.route("/").post(registerUser)
-router.post("/login", authUser)
-router.route("/profile").get(protect, getUserProfile)
+router.route("/").post(registerUser);
+router.post("/login", authUser);
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile);
 
 export default router;
