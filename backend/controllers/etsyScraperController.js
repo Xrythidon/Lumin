23392de0641c2e9dcea1f11b83291b9716046545
Alex Danisz <alexdanisz@live.com>
2021-04-15 15:02:54 -0700

    Added getReviewByProductId etsy route

--- backend/controllers/etsyScraperController.js
+++ backend/controllers/etsyScraperController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
 import axios from "axios";
+import qs from "qs";
 import got from "got";
 import Crawler from "crawler";
 import jsdom from "jsdom";
@@ -19,10 +20,6 @@ const scrapeAllProducts = asyncHandler(async (req, res) => {
   const parentContainerDiv = dom.querySelector(".featured-listings");
   const elements = parentContainerDiv.querySelectorAll("a");
 
-
-
-
-
   elements.forEach((element, index) => {
     const productUrl = element.getAttribute("href");
     axios
@@ -41,7 +38,7 @@ const scrapeAllProducts = asyncHandler(async (req, res) => {
   res.json(products);
 });
 
-const crawlHelper = (fn, ) => {
+const crawlHelper = (fn) => {
   const c = new Crawler({
     jQuery: jsdom,
     rateLimit: 5000,
@@ -59,8 +56,7 @@ const crawlHelper = (fn, ) => {
 
   c.queue(etsyUrl);
   c.queue(etsyUrl);
-}
-
+};
 
 // @desc    Scrape all etsy products
 // @route   get /api/scrape/test
@@ -72,47 +68,44 @@ const testScrape = (req, res) => {
     const elements = parentContainerDiv.querySelectorAll("a");
     const products = [];
 
-      const c = new Crawler({
-        jQuery: jsdom,
-        rateLimit: 2000,
-        maxConnections: 1,
-        callback: function (error, res, done) {
-          if (error) {
-            console.log(error);
-          } else {
-            const pageDom = new JSDOM(res.body.toString()).window.document;
-            let product = {};
-            product.url = res.options.uri;
-            product.title = pageDom.querySelector("h1[data-buy-box-listing-title]").textContent.trim();
-            const picture = pageDom.querySelector("ul[data-carousel-pane-list]");
-            const pictureArray = picture.querySelectorAll("img");
-            const productImageArray = [];
-            pictureArray.forEach((pic) => {
-              pic.getAttribute("src")
-                ? productImageArray.push(pic.getAttribute("src"))
-                : productImageArray.push(pic.getAttribute("data-src"));
-            });
-            product.images = productImageArray;
-            console.log(product);
-            products.push(product);
-          }
-          done();
-        },
-      });
-
-
-      elements.forEach((element) => {
-        const productUrl = element.getAttribute("href");
-        c.queue({
-          uri: productUrl
-      })
-  
+    const c = new Crawler({
+      jQuery: jsdom,
+      rateLimit: 2000,
+      maxConnections: 1,
+      callback: function (error, res, done) {
+        if (error) {
+          console.log(error);
+        } else {
+          const pageDom = new JSDOM(res.body.toString()).window.document;
+          let product = {};
+          product.url = res.options.uri;
+          product.title = pageDom.querySelector("h1[data-buy-box-listing-title]").textContent.trim();
+          const picture = pageDom.querySelector("ul[data-carousel-pane-list]");
+          const pictureArray = picture.querySelectorAll("img");
+          const productImageArray = [];
+          pictureArray.forEach((pic) => {
+            pic.getAttribute("src")
+              ? productImageArray.push(pic.getAttribute("src"))
+              : productImageArray.push(pic.getAttribute("data-src"));
+          });
+          product.images = productImageArray;
+          console.log(product);
+          products.push(product);
+        }
+        done();
+      },
     });
-    c.on('drain',function(){
+
+    elements.forEach((element) => {
+      const productUrl = element.getAttribute("href");
+      c.queue({
+        uri: productUrl,
+      });
+    });
+    c.on("drain", function () {
       // For example, release a connection to database.
       res.json(products);
-  });
-
+    });
   });
 };
 
@@ -138,8 +131,68 @@ const testCrawler = (req, res) => {
   c.queue(etsyUrl);
   c.queue(etsyUrl);
 
-  res.json("done with crawling")
+  res.json("done with crawling");
 };
 
-export { scrapeAllProducts, testScrape, testCrawler };
+// @desc    Scrape all reviews by etsy product
+// @route   get /api/scrape/review/:id
+// @access  Public
+const scrapeAllReviewsByProduct = asyncHandler(async (req, res) => {
+  
+  const listing_id = req.params.id;
+  const page = Number(req.query.page) || 1;
 
+  const data = qs.stringify({
+    "specs[reviews][0]": " Listzilla_ApiSpecs_Reviews",
+    "specs[reviews][1][listing_id]": ` ${listing_id}`,
+    "specs[reviews][1][shop_id]": " 11050902",
+    "specs[reviews][1][render_complete]": " true",
+    "specs[reviews][1][active_tab]": " same_listing_reviews",
+    "specs[reviews][1][listing_price]": " 60",
+    "specs[reviews][1][should_lazy_load_images]": " false",
+    "specs[reviews][1][is_sold_out_listing]": " false",
+    "specs[reviews][1][should_use_pagination]": " true",
+    "specs[reviews][1][page]": ` ${page}`,
+    "specs[reviews][1][should_show_variations]": " true",
+    "specs[reviews][1][selected_keyword_filter]": " all",
+    "specs[reviews][1][is_external_landing]": " false",
+    "specs[reviews][1][is_reviews_untabbed_cached]": " false",
+    "specs[reviews][1][search_query]": " ",
+    "specs[reviews][1][sort_option]": " Relevancy",
+  });
+  const config = {
+    method: "post",
+    url: "https://www.etsy.com/api/v3/ajax/bespoke/member/neu/specs/reviews",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      cookie:
+        " user_prefs=2w1wISmoqyPnSpQxc3YX21RCvy1jZACChFBxGxgdreTs6KKkk1eak6OjlJqn6-6kpAMUgooYQShcRCwDAA..; fve=1616189244.0; ua=531227642bc86f3b5fd7103a0c0b4fd6; uaid=9TpVVhgNGnO6yjaf60XUf4OGgW1jZACChFBxGzCdKudZrVSamJmiZKXk5BNRkJ2YEVDhUxnhlhQUkG1qEeWd4u-XElBVoFTLAAA.; G_ENABLED_IDPS=google; pla_spr=0|0; last_browse_page=https%3A%2F%2Fwww.etsy.com%2Fca%2Fshop%2Fshopluminjewelry; perf=wf:1; exp_hangover=tL6Io0lVT2H4CTLJXuECLUQ2VUVjZACChFBxGzCdZ8derVSemhSfWFSSmZaZnJmYE5-TWJKal1wZX2gSb2RgaKlkpZSZl5qTmZ6ZlJOqVMsAAA..; exp_hangover=CAFxmo4H5xJtkVfaO2LGJmi54H1jZACChFBxGzBdsbmzWqk8NSk-sagkMy0zOTMxJz4nsSQ1L7kyvtAk3sjA0FLJSikzLzUnMz0zKSdVqZYBAA..; fve=1617821682.0; uaid=M8kLR0c2iIdScbXnKvksZfPTe0BjZACChFBxGzBdsbmzWqk0MTNFyUrJySeiIDsxI6DCpzLCLSkoINvUIso7xd8vJaCqQKmWAQA.; user_prefs=s4lVCA50xVmKF6R_xFETlqEGgwNjZACChNz_n2B0tJKzo4uSTl5pTo6OUmqerruTkg5QCCpiBKFwEbEMAA..",
+      origin: " https://www.etsy.com",
+      referer:
+        " https://www.etsy.com/ca/listing/675362615/dainty-necklace-set-14k-gold-filled?ref=shop_home_feat_4&frs=1",
+      "sec-ch-ua": ' "Google Chrome";v="89", "Chromium";v="89", ";Not A Brand";v="99"',
+      "sec-ch-ua-mobile": " ?0",
+      "sec-fetch-dest": " empty",
+      "sec-fetch-mode": " cors",
+      "sec-fetch-site": " same-origin",
+      "user-agent":
+        " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36",
+      "x-csrf-token":
+        " 3:1617837567:6GZh6KPxBCpY62n3FCS9mg5qxB9t:f3425f085c5a9b251e8a81ff0f023d8a015877e199b723f04097db9afcc7c2b9",
+      "x-detected-locale": " CAD|en-GB|CA",
+      "x-page-guid": " eb6d255cdf3.0917c6e284f266ece861.00",
+      "x-requested-with": " XMLHttpRequest",
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      res.json(response.data.output.reviews);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+});
+
+export { scrapeAllProducts, testScrape, testCrawler, scrapeAllReviewsByProduct };
--- backend/routes/etsyRoutes.js
+++ backend/routes/etsyRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 const router = express.Router();
-import {scrapeAllProducts, testScrape, testCrawler} from "../controllers/etsyScraperController.js"
+import {scrapeAllProducts, testScrape, testCrawler, scrapeAllReviewsByProduct} from "../controllers/etsyScraperController.js"
 
 
 // route -> api/scrape/
 router.route("/").get(scrapeAllProducts);
 router.route("/test").get(testScrape);
 router.route("/crawler").get(testCrawler);
+router.route("/reviews/:id").get(scrapeAllReviewsByProduct);
 
 export default router;
